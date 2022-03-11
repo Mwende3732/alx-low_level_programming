aa320e1e8da2a3ac425af5e4d88ebedc7717d115
Mwende <muthianisharlinemwende@gmail.com>
2022-03-11 12:29:25 +0300

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-/** this is a short description for a program to print plain text and file size */
+/** this is a c program */
 #include <stdio.h>
 /**
  * main: Prints out the sizes of the various data types on the computer
@@ -13,16 +13,11 @@ int main(void)
 	long long ll;
 	float f;
 
-	printf("Size of a char: %lu", (unsigned long)sizeof(c));
-	printf(" byte(s)\n");
-	printf("Size of an int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long int: %lu", (unsigned long)sizeof(l));
-	printf(" byte(s)\n");
-	printf("Size of a long long int: %lu", (unsigned long)sizeof(ll));
-	printf(" byte(s)\n");
-	printf("Size of a float: %lu", (unsigned long)sizeof(f));
-	printf(" byte(s)\n");
-    return (0);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
 
