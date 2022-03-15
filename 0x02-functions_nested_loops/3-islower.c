fb0d33c1e232eb9c194f5ad44d1b0db809b9ea26
Mwende <muthianisharlinemwende@gmail.com>
2022-03-15 17:56:29 +0300

    3 is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - filters main input
+ * @c: char
+ * Return: 1 when true, 0 for all else
+ */
+int _islower(int c)
+{
+	
+	if (c > 91)
+		return (1);
+	else
+		return (0);
+}
