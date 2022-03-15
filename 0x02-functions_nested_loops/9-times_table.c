b4be7ceaca033c947c985eb261cb7bad4a9248eb
Mwende <muthianisharlinemwende@gmail.com>
2022-03-15 23:24:14 +0300

    nine times nine table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - writes the 9 times table
+ * @void: void
+ * Return: void
+ */
+void times_table(void)
+{
+	int col, row, mul;
+
+	for (col = 0; col < 10; col++)
+	{
+		for (row = 0; row < 10; row++)
+		{
+			mul = col * row;
+			if (mul > 9)
+			{
+				_putchar('.');
+				_putchar(' ');
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+			}
+			else
+			{
+				if (row != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(mul + '0');
+			}
+		}
+		_putchar('\n');
+	}}
