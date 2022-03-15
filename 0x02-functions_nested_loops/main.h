daf102d53e619ad18c28a20b9f2a03ccf384afc9
Mwende <muthianisharlinemwende@gmail.com>
2022-03-15 17:20:29 +0300

    prototypes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,13 @@
-../main.h
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int_islower(int c);
+int_isalpha(int c);
+int print_sign(int c);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void times_table(void);
+void print_to_98(int n);
+void print_times_table(int n);
