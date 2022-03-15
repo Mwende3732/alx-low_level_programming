eca1b44c7c1b97c411ba3ed571dc161c187c17bb
Mwende <muthianisharlinemwende@gmail.com>
2022-03-15 18:04:40 +0300

    3 main

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the main code
+ * Return:0 on success
+ */
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
