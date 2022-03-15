281ae8b03ff881aef0ee5e38684a3b45596fd308
Mwende <muthianisharlinemwende@gmail.com>
2022-03-15 13:11:41 +0300

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "_putchar.h"
+/**
+ * main - Prints _putchar followed by new line in computer.
+ * Return: 0 on success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
