d3d16a432d93b356808ed3a692c6177fa6d1e9b3
Mwende <muthianisharlinemwende@gmail.com>
2022-03-17 12:36:36 +0300

    first task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
  * main - tests function with 0
- *
- * Description: testing with zero
- *
- * Return: 0 on success
- *
+ *Return: 0
  */
+
 int main(void)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	positive_or_negative(i);
+	
 	return (0);
 }
