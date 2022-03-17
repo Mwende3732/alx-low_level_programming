a87842bac4c35b871c5d26a698e9f7a692b178c5
Mwende <muthianisharlinemwende@gmail.com>
2022-03-17 10:42:53 +0300

    test if positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - tests function with 0
+ *
+ * Description: testing with zero
+ *
+ * Return: 0 on success
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
