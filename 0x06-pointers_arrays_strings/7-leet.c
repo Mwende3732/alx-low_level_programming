69e939429ae0b1a7474c3b59b6dead76f6fe8e49
Mwende <muthianisharlinemwende@gmail.com>
2022-03-24 13:25:24 +0300

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,12 +7,12 @@
 
 char *leet(char *s)
 {
-	int count = , i;
+	int count = 0, i;
 	int low_letters[] = {97, 101, 111, 116, 108};
 	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
-	while (*(s + count) != '\10')
+	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
@@ -24,6 +24,6 @@ char *leet(char *s)
 		}
 		count++;
 	}
-	
+
 	return (s);
 }
