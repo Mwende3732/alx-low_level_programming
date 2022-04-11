33dff07311c7a76e6bba5f3e7985a3a4209d6a6d
Mwende <muthianisharlinemwende@gmail.com>
2022-04-11 22:54:33 +0300

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct that stores some info of a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* !DOG_H */
