c406bc56515aa6e3f02c319a3198ac4313a96df4
Mwende <muthianisharlinemwende@gmail.com>
2022-06-15 16:24:18 +0300

    task

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget -P /tmp/ https://raw.github.com/luischaparroc/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
+© 2022 GitHub, Inc.
+Terms
+Privacy
+[A[C[C[C[C[C[A[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[
